57203086ec80a9d0244b94ef52d92c1f6c3c34c3
Alextufeng <salman-aka@mail.ru>
2022-02-24 22:27:54 +0300

    feat: write function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    
+    while(n>0){
+     result = result + (n%10);
+     n = parseInt(n/10);
+    }
+    
+    return result;
 }
+
+//console.log(reverse(n));
